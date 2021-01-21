fbb5c93b7eacc16b9a804fc212acd7ac7238ac6c
Ivan Seryakov <vambafool@bk.ru>
2021-01-21 21:28:47 +0300

    test webpack abilities

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
-console.log('Hello world!')
+import AppService from './modules/app.service.js'
+import {config} from './modules/config.js'
+import './modules/header.component'
+
+console.log('Config key: ', config.key)
+
+const service = new AppService('Hello world!')
+service.log()
