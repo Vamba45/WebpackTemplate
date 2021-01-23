78fa23c61fddc4a438e3bf0e4e62e78567511520
Ivan Seryakov <vambafool@bk.ru>
2021-01-23 23:49:26 +0300

    add modules for work with css

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import AppService from './modules/app.service.js'
 import {config} from './modules/config.js'
 import './modules/header.component'
 
+import './css/main.css'
+
 console.log('Config key: ', config.key)
 
 const service = new AppService('Hello world!')
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,7 @@
 const path = require('path')
 const HTMLPlugin = require('html-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin')
 
 module.exports = {
     entry: './src/index.js',
@@ -7,10 +9,26 @@ module.exports = {
         filename: 'bundle.js',
         path: path.resolve(__dirname, 'dist')
     },
+    optimization: {
+        minimizer: [
+            new OptimizeCssAssetsPlugin()
+        ]
+    },
     plugins: [
         new HTMLPlugin({
             filename: 'index.html',
             template: './src/index.html'
+        }),
+        new MiniCssExtractPlugin({
+            filename: 'style.css'
         })
-    ]
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: [MiniCssExtractPlugin.loader, 'css-loader']
+            }
+        ]
+    }
 }
